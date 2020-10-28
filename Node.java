f0de6215895b8917fa46a33e68a874d6947e9f71
Deepanshu Rohilla <52520071+Deepanshu-Rohilla@users.noreply.github.com>
2020-10-28 09:11:42 +0530

    nodes

---
+++ Node.java
@@ -0,0 +1,9 @@
+class Node{
+	int data;
+	Node left,right;
+	Node(int data){
+		this.data = data;
+		this.left=null;
+		this.right= null;
+	}
+}
---
+++ TreeFromTraversal.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class TreeFromTraversal{
+	public Node findTree(int preOrder[], int inOrder[]){
+
+
+	}
+	public static void main(String[] args) {
+		int preOrder[] = {1,2,3,4,6,7,5};
+		int inOrder[] = {3,2,6,4,7,1,5};
+		Tree answer = new Tree(findTree(preOrder[],inOrder[]));	
+	}
+}
